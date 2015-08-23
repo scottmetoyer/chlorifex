75b02c2aed26dd42b2b8a4c85cef6ca030fd72df
Scott Metoyer <scott.metoyer@gmail.com>
2015-08-22 19:38:16 -0700

    Initial commit

---
+++ Chlorifex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chlorifex
+//
+//  Created by Scott Metoyer on 8/22/15.
+//  Copyright (c) 2015 Scott Metoyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
