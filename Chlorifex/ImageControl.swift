b26bc4a1f9c9e276f918acb235b2310d092959eb
Scott Metoyer <scott.metoyer@gmail.com>
2015-09-03 20:14:46 -0700

    Got basic image dragging working

--- Chlorifex/ImageControl.swift
+++ Chlorifex/ImageControl.swift
@@ -9,19 +9,33 @@
 import UIKit
 
 class ImageControl: UIView {
+    var location = CGPoint(x: 20, y: 20)
+    var imageView = UIImageView()
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
     required init(image anImage: UIImage) {
-        let rect = CGRectMake(60, 60, anImage.size.width / 4, anImage.size.height / 4)
+        let rect = CGRectMake(location.x, location.y, anImage.size.width / 4, anImage.size.height / 4)
         super.init(frame: rect)
         
-        let imageView = UIImageView(image: anImage)
+        imageView = UIImageView(image: anImage)
         imageView.frame = rect
         imageView.contentMode = .ScaleAspectFit
+        self.center = location;
         addSubview(imageView)
-        
     }
     
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        var touch : UITouch! = touches.first as! UITouch
+        location = touch.locationInView(self)
+        self.center = location
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        var touch : UITouch! = touches.first as! UITouch
+        location = touch.locationInView(self)
+        self.center = location
+    }
 }
